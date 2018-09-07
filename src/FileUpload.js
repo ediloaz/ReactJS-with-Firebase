6b4c9735ca6c1f9e2a7aa2f770a59b1754b9df86
Unknown <ediloaz@gmail.com>
2018-09-06 21:49:47 -0600

    Progress bar programmed

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Firebase from 'firebase';
+import firebase from 'firebase';
 
 class FileUpload extends Component{
   constructor () {
@@ -8,6 +8,28 @@ class FileUpload extends Component{
       uploadValue : 0,
       picture : null
     };
+
+    this.handleUpload = this.handleUpload.bind(this);
+  }
+
+  handleUpload (event) {
+    const file = event.target.files[0];    // Se obtiene el fichero que se acaba de subir
+    const storageRef = firebase.storage().ref(`/fotos/${file.name}`);
+    const task = storageRef.put(file);
+
+    task.on('state_changed', snapshot => {  // es un estado de Firebase, no de React, se activa al subir el file
+      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      this.setState({
+        uploadValue : percentage
+      })
+    }, error => {
+      console.log(error.message)
+    }, () => {
+      this.setState ({
+        uploadValue : 100,
+        picture : task.snapshot.downloadURL
+      });
+    });
   }
 
   render () {
@@ -15,7 +37,7 @@ class FileUpload extends Component{
       <div>
         <progress value={this.state.uploadValue} max="100"></progress>
         <br/>
-        <input type="file" onChange={this.props.onUpload} />
+        <input type="file" onChange={this.handleUpload} />
         <br/>
         <img width="300" src={this.state.picture} alt="" />
       </div>
